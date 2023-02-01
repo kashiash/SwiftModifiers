acf5bb799ab04538ed55a60a69da3f6649e703d3
Jacek Kosinski <kashiash@gmail.com>
2023-02-02 00:18:40 +0100

    Initial Commit

---
+++ SwiftModifiers/SwiftModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftModifiersApp.swift
+//  SwiftModifiers
+//
+//  Created by Jacek Kosiński G on 02/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
